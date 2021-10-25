8340ac8b590b87521af5ff8d80e5856be6d0370f
JiangTaoHou <925147954@qq.com>
2021-10-25 22:44:05 +0800

    MyBatis整合成功

---
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/mapper/AppUserMapper.java
@@ -0,0 +1,8 @@
+package com.imooc.user.mapper;
+
+import com.imooc.my.mapper.MyMapper;
+import com.imooc.pojo.AppUser;
+import org.apache.ibatis.annotations.Mapper;
+
+public interface AppUserMapper extends MyMapper<AppUser> {
+}
---
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/mapper/FansMapper.java
@@ -0,0 +1,8 @@
+package com.imooc.user.mapper;
+
+import com.imooc.my.mapper.MyMapper;
+import com.imooc.pojo.Fans;
+import org.apache.ibatis.annotations.Mapper;
+
+public interface FansMapper extends MyMapper<Fans> {
+}
