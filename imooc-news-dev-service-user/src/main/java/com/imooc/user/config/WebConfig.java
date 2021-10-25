c6f0c553c5a3305112a000787ffe35a5b304a289
JiangTaoHou <925147954@qq.com>
2021-10-26 00:29:24 +0800

    整合swagger,在主启动类上贴了@EnableSwagger2才出来，但是，还是有错误的

---
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/config/WebConfig.java
@@ -0,0 +1,27 @@
+//package com.imooc.user.config;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    /**
+//     * 静态资源配置
+//     * @param registry
+//     */
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        //Swagger2
+//        registry.addResourceHandler("/swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+//}
--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.imooc.user.controller;
 
 import com.imooc.api.controller.user.HelloControllerApi;
 import com.imooc.grace.result.GraceJSONResult;
-import com.imooc.grace.result.ResponseStatusEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
