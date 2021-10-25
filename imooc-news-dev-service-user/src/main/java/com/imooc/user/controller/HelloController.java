daa8f9ca17f38bec25634df3165424c6cb86768a
JiangTaoHou <925147954@qq.com>
2021-10-25 16:57:21 +0800

    api接口暴露，就是把@GetMapping这些东西弄到API工程了，实现还是在原来的里面。

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
@@ -1,11 +1,11 @@
 package com.imooc.user.controller;
 
+import com.imooc.api.controller.user.HelloControllerApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @GetMapping("/hello")
+public class HelloController implements HelloControllerApi {
     public Object hello(){
         return "hello";
     }
