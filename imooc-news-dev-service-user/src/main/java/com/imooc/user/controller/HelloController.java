cc3f3e0a6968412bc5b076b0285518b686ff9e0f
JiangTaoHou <925147954@qq.com>
2021-10-25 17:08:18 +0800

    配置logback日志与多环境profile

--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.imooc.user.controller;
 
 import com.imooc.api.controller.user.HelloControllerApi;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController implements HelloControllerApi {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     public Object hello(){
         return "hello";
     }
