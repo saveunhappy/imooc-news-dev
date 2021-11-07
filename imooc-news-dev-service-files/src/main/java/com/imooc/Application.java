2ff35d42ed4d8667d423dc0ec4155cda5b68b6ee
JiangTaoHou <925147954@qq.com>
2021-11-07 22:53:04 +0800

    创建文件服务module

---
+++ imooc-news-dev-service-files/src/main/java/com/imooc/Application.java
@@ -0,0 +1,14 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ imooc-news-dev-service-files/src/main/java/com/imooc/files/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.imooc.files.controller;
+
+import com.imooc.api.controller.user.HelloControllerApi;
+import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.utils.RedisOperator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HelloController implements HelloControllerApi {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    public Object hello(){
+        return GraceJSONResult.ok("hello world!!!");
+    }
+}
