6512cac925e92697978d79afca51ae7190d8acfb
JiangTaoHou <925147954@qq.com>
2021-11-17 22:26:38 +0800

    设置时间日期转换配置

---
+++ imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/AdminUserMngControllerApi.java
@@ -0,0 +1,29 @@
+package com.imooc.api.controller.user;
+
+import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.pojo.bo.UpdateUserInfoBO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.Date;
+
+@Api(value = "用户管理相关的接口定义",tags = {"用户管理相关的接口controller"})
+@RequestMapping("appUser")
+public interface AdminUserMngControllerApi {
+
+    @ApiOperation(value = "查询所有网站用户",notes = "查询所有网站用户",httpMethod = "POST")
+    @PostMapping("/queryAll")
+    GraceJSONResult getUserInfo(@RequestParam("nickname") String nickname,
+                                @RequestParam("status") Integer status,
+                                @RequestParam("startDate") Date startDate,
+                                @RequestParam("endDate") Date endDate,
+                                @RequestParam("page") Integer page,
+                                @RequestParam("pageSize") Integer pageSize);
+
+
+}
---
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/AdminUserMngController.java
@@ -0,0 +1,39 @@
+package com.imooc.user.controller;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.imooc.api.BaseController;
+import com.imooc.api.controller.user.AdminUserMngControllerApi;
+import com.imooc.api.controller.user.HelloControllerApi;
+import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.utils.RedisOperator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@RestController
+public class AdminUserMngController extends BaseController implements AdminUserMngControllerApi {
+    private static final Logger logger = LoggerFactory.getLogger(AdminUserMngController.class);
+
+
+    @Override
+    public GraceJSONResult getUserInfo(String nickname,
+                                       Integer status,
+                                       Date startDate,
+                                       Date endDate,
+                                       Integer page,
+                                       Integer pageSize) {
+        System.out.println(startDate);
+        System.out.println(endDate);
+        if(page == null){
+            page = COMMON_START_PAGE;
+        }
+        if(pageSize == null){
+            pageSize = COMMON_PAGE_SIZE;
+        }
+        return null;
+    }
+}
