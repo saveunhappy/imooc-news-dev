6b0965d49250b8ff4c7731f57d0bffe76e71e18a
JiangTaoHou <925147954@qq.com>
2021-11-22 22:11:45 +0800

    结合动态数据生成静态化HTML

--- imooc-news-dev-service-article/src/main/java/com/imooc/article/controller/FreemarkerController.java
+++ imooc-news-dev-service-article/src/main/java/com/imooc/article/controller/FreemarkerController.java
@@ -1,22 +1,63 @@
 package com.imooc.article.controller;
 
-import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.pojo.Article;
 import com.imooc.pojo.Spouse;
 import com.imooc.pojo.Stu;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.apache.commons.lang3.CharSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @Controller
 @RequestMapping("/free")
 public class FreemarkerController {
     private static final Logger logger = LoggerFactory.getLogger(FreemarkerController.class);
+    @Value("${freemarker.html.target}")
+    private String htmlTarget;
+    @ResponseBody
+    @GetMapping("/createHtml")
+    public String createHtml(Model model) throws Exception {
+        //0.配置freemarker基本环境
+        Configuration cfg = new Configuration(Configuration.getVersion());
+        String classPath = this.getClass().getResource("/").getPath();
+        cfg.setDirectoryForTemplateLoading(new File(makePathAndSubstring(classPath + "templates")));
+        System.out.println(htmlTarget.replace("\\",File.separator));
+        System.out.println(makePathAndSubstring(classPath + "templates"));
+        String stranger = "world";
+        model.addAttribute("there",stranger);
+        //1.获得现有的模板ftl文件
+        Template template = cfg.getTemplate("stu.ftl", StandardCharsets.UTF_8.name());
+        //2.获取动态数据
+        model = makeModel(model);
+        //3.融合动态数据ftl,生成HTML
+        File tempDic = new File(htmlTarget);
+        if(!tempDic.exists()){
+            tempDic.mkdirs();
+        }
+        Writer out = new FileWriter(htmlTarget + File.separator + "10010.html");
+        template.process(model,out);
+        out.close();
+        return "ok";
+    }
+
+    private String makePathAndSubstring(String path){
+        return path.substring(1).replace("/",File.separator);
+    }
     @GetMapping("/hello")
     public Object hello(Model model){
         String stranger = "world";
