1e92e60a3592a42e9c98f1e1973d0f68e6d16ebd
JiangTaoHou <925147954@qq.com>
2021-11-25 21:58:45 +0800

    实现随机负载均衡

--- imooc-news-dev-service-article/src/main/java/com/imooc/article/Application.java
+++ imooc-news-dev-service-article/src/main/java/com/imooc/article/Application.java
@@ -1,8 +1,10 @@
 package com.imooc.article;
 
+import com.rule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -10,6 +12,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.imooc.article.mapper")
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
 @EnableEurekaClient
+//@RibbonClient(name="SERVICE-USER",configuration = MyRule.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
