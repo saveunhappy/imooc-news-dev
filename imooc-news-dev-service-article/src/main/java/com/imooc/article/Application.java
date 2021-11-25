f781a4bb176af87530a6e1c65d80e06dfe17bdb6
JiangTaoHou <925147954@qq.com>
2021-11-25 14:25:57 +0800

    实现用户与文章的服务注册

--- imooc-news-dev-service-article/src/main/java/com/imooc/article/Application.java
+++ imooc-news-dev-service-article/src/main/java/com/imooc/article/Application.java
@@ -2,12 +2,14 @@ package com.imooc.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.article.mapper")
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
