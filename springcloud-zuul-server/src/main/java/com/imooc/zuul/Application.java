b82a6eac263da292c2edd37f2024b25c9e17f333
JiangTaoHou <925147954@qq.com>
2021-11-26 22:53:54 +0800

    配置微服务实例的路由

--- springcloud-zuul-server/src/main/java/com/imooc/zuul/Application.java
+++ springcloud-zuul-server/src/main/java/com/imooc/zuul/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
         MongoAutoConfiguration.class, RabbitAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
 @EnableZuulProxy
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
